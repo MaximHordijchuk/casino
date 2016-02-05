893ff05738fc29ce964f3c464dfed28f5eda5400
max <hordijchuk.m.i@gmail.com>
2016-02-05 13:46:23 +0200

    Add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :poker_tables
+
+  before_validation :downcase_email
+  validates :email, email: true, presence: true, uniqueness: true
+
+  private
+
+  def downcase_email
+    self.email = self.email.downcase
+  end
 end
