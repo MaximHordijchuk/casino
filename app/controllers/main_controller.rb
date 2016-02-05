b83a41f122fd1d689029574071d73cd78a303b37
max <hordijchuk.m.i@gmail.com>
2016-02-05 19:35:37 +0200

    Add notifications. Put views into container

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,7 +9,11 @@ class MainController < ApplicationController
     respond_to do |format|
       if @user.persisted? || @user.save
         add_poker_tables
-        format.html { redirect_to '/', notice: 'Poker tables were successfully added.' }
+        if @tables.empty?
+          format.html { redirect_to '/', alert: 'No poker tables were added' }
+        else
+          format.html { redirect_to '/', notice: poker_table_notice }
+        end
       else
         format.html { redirect_to '/', alert: 'Could not create user' }
       end
@@ -18,15 +22,24 @@ class MainController < ApplicationController
 
   private
 
+  def poker_table_notice
+    'Poker tables ' << @tables.map { |table| table.name }.join(', ') << ' were successfully added.'
+  end
+
   def set_user
     @user = User.find_or_initialize_by(user_params)
   end
 
   def add_poker_tables
+    @tables = []
     if !params[:poker_tables].nil? && !params[:poker_tables][:id].nil?
       tables = params[:poker_tables][:id].map { |id, checked| id.to_i if checked == '1' }.flatten
       tables &= PokerTable.available.pluck(:id).to_a
-      tables.each { |table_id| @user.poker_tables << PokerTable.find(table_id) }
+      tables.each do |table_id|
+        table = PokerTable.find(table_id)
+        @tables << table
+        @user.poker_tables << table
+      end
     end
   end
 
