35a2602c00955b2947fbf5308fc743977eb85958
max <hordijchuk.m.i@gmail.com>
2016-02-05 15:22:50 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+PokerTable.destroy_all
+
+User.create! [
+    { email: 'vasya@gmail.com' },
+    { email: 'anton@mail.ru' },
+    { email: 'vlad@i.ua' },
+    { email: 'denis1993@yandex.ru' },
+    { email: 'demedovich@gmail.com' },
+    { email: 'andrey@gmail.com' },
+    { email: 'ivan@i.ua' },
+             ]
+
+10.times do |i|
+  PokerTable.create!({ name: "Poker Table #{i}",
+                       start_time: Time.zone.now + rand(5).minutes })
+end
+PokerTable.create!({ name: "Poker Table 11",
+                     start_time: Time.zone.now - 1.minutes})
+
+PokerTable.first.users << User.all.limit(6)
